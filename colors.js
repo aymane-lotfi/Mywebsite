d14f910d03bebd162240858e91f0a2f6d1d66a3a
aymane lotfi <31337952+aymane-lotfi@users.noreply.github.com>
2018-03-20 17:45:48 +0000

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,30 @@
+// Define some colours
+var colours = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple"    
+];
+
+// Retrieve the words
+var text = $("h1").html().split(" ");
+
+// Empty the elment
+$("p").empty();
+
+// Iterate over the words
+$.each(text, function(i, word) {
+    if(i != text.length) {
+        word += " "; // Add space after word 
+    }
+
+     // Get random color
+    var colourIndex = Math.floor(Math.random() * colours.length);
+
+    $("<span>")
+        .html(word)
+        .css("color", colours[colourIndex])
+        .appendTo($("h1"));
+});
